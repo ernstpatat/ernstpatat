f0ffe78bd14db93feb615e3d3ee60d9c55816d76
ernstpatat <61903918+ernstpatat@users.noreply.github.com>
2023-09-03 13:26:40 +0200

    Add files via upload

--- KeyListener.js
+++ KeyListener.js
@@ -1,7 +1,5 @@
 addEventListener("keydown", function(e){
-    if (e.code == "Space") vy = -5
-})
-
-addEventListener("keyup", function(e){
-    if (e.code == "Space") vy = 0;
+    if (grounded == true){
+        if (e.code == "Space") vy = -9, grounded = false
+    }
 })
--- app.js
+++ app.js
@@ -1,30 +1,51 @@
 console.log("app.js loaded")
-
 const canvas = document.getElementById("canvas")
 const ctx = canvas.getContext("2d")
-
-let x = 0
-let y = 0
+let x = 850
+let y = canvas.height - 100
 let vx = 0
 let vy = 0
+let grounded = false
+let Cactusx = canvas.width
+let Cactusy = canvas.height - 50
+const button1 = document.getElementById("button")
+Dead = false
+let Score = 0
 
-function update(){
-    ctx.clearRect(0, 0, canvas.width, canvas.height)
-    x += vx
-    y += vy
-    ctx.fillRect(x, y, 50, 50)
-    collision()
-    requestAnimationFrame(update)
+function Start() {
+    Dead = false
+    Score = 0
+    button1.disabled = true
+    update()
 }
 
-function collision() {
-    if (y>= canvas.height - 100){
-        y = canvas.height - 100
+function update() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    y += vy
+    collision()
+    ctx.fillRect(x, y, 50, 100)
+    ctx.fillRect(Cactusx, Cactusy, 50, 50)
+    if (Cactusx <= -50) Cactusx = canvas.width, Score += 1
+    document.getElementById("score").innerHTML = Score
+    if (Cactusx <= x) if (x <= Cactusx + 50) if (Cactusy <= y + 50) if (y + 50 <= Cactusy + 50) Cactusx = canvas.width, button1.disabled = false, Dead = True
+    if (Cactusx <= x + 50) if (x + 50 <= Cactusx + 50) if (Cactusy <= y + 100) if (y + 50 <= Cactusy + 100) Cactusx = canvas.width, button1.disabled = false, Dead = True
+    if (Cactusx <= x + 50) if (x + 50 <= Cactusx + 50) if (Cactusy <= y + 50) if (y + 50 <= Cactusy + 50) Cactusx = canvas.width, button1.disabled = false, Dead = True
+    if (Cactusx <= x) if (x <= Cactusx + 50) if (Cactusy <= y + 100) if (y + 50 <= Cactusy + 100) Cactusx = canvas.width, button1.disabled = false, Dead = True
+    Cactusx -= 5
+    if (Dead != true) {
+    requestAnimationFrame(update)
     }
 }
 
-update()
+function collision() {
+    if (y >= canvas.height - 100){
+    y = canvas.height - 100
+    grounded = true
+    }
+}
 
 setInterval(function Gravity() {
+    if (grounded == false){
     vy += 0.05
+    }
 })
