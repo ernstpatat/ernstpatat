7b1873478fbceae36468a96492a0f1d96993b79c
ernstpatat <61903918+ernstpatat@users.noreply.github.com>
2023-09-07 23:29:01 +0200

    Add files via upload

--- KeyListener.js
+++ KeyListener.js
@@ -1,5 +1,4 @@
 addEventListener("keydown", function(e){
-    if (grounded == true){
-        if (e.code == "Space") vy = -9, grounded = false
-    }
+    if (grounded == true) if (e.code == "Space") vy = -9, grounded = false
 })
+
--- app.js
+++ app.js
@@ -11,11 +11,30 @@ let Cactusy = canvas.height - 50
 const button1 = document.getElementById("button")
 Dead = false
 let Score = 0
+Paused = false
+buttonPause.disabled = true
+buttonUnPause.disabled = true
+CactusSpeed = 4
+
+function Pause() {
+    buttonPause.disabled = true
+    buttonUnPause.disabled = false
+    Paused = true
+}
+
+function UnPause() {
+    buttonPause.disabled = false
+    buttonUnPause.disabled = true
+    Paused = false
+    if (Dead == false) update()
+}
 
 function Start() {
     Dead = false
     Score = 0
     button1.disabled = true
+    buttonPause.disabled = false
+    CactusSpeed = 4
     update()
 }
 
@@ -27,12 +46,14 @@ function update() {
     ctx.fillRect(Cactusx, Cactusy, 50, 50)
     if (Cactusx <= -50) Cactusx = canvas.width, Score += 1
     document.getElementById("score").innerHTML = Score
-    if (Cactusx <= x) if (x <= Cactusx + 50) if (Cactusy <= y + 50) if (y + 50 <= Cactusy + 50) Cactusx = canvas.width, button1.disabled = false, Dead = True
-    if (Cactusx <= x + 50) if (x + 50 <= Cactusx + 50) if (Cactusy <= y + 100) if (y + 50 <= Cactusy + 100) Cactusx = canvas.width, button1.disabled = false, Dead = True
-    if (Cactusx <= x + 50) if (x + 50 <= Cactusx + 50) if (Cactusy <= y + 50) if (y + 50 <= Cactusy + 50) Cactusx = canvas.width, button1.disabled = false, Dead = True
-    if (Cactusx <= x) if (x <= Cactusx + 50) if (Cactusy <= y + 100) if (y + 50 <= Cactusy + 100) Cactusx = canvas.width, button1.disabled = false, Dead = True
-    Cactusx -= 5
-    if (Dead != true) {
+    if (Cactusx <= x) if (x <= Cactusx + 50) if (Cactusy <= y + 50) if (y + 50 <= Cactusy + 50) Cactusx = canvas.width, button1.disabled = false, Dead = true, buttonPause.disabled = true
+    if (Cactusx <= x + 50) if (x + 50 <= Cactusx + 50) if (Cactusy <= y + 100) if (y + 50 <= Cactusy + 100) Cactusx = canvas.width, button1.disabled = false, Dead = true, buttonPause.disabled = true
+    if (Cactusx <= x + 50) if (x + 50 <= Cactusx + 50) if (Cactusy <= y + 50) if (y + 50 <= Cactusy + 50) Cactusx = canvas.width, button1.disabled = false, Dead = true, buttonPause.disabled = true
+    if (Cactusx <= x) if (x <= Cactusx + 50) if (Cactusy <= y + 100) if (y + 50 <= Cactusy + 100) Cactusx = canvas.width, button1.disabled = false, Dead = true, buttonPause.disabled = true
+    Cactusx -= CactusSpeed
+    ScoreString = Score.toString()
+    if (Score >= 10) if (ScoreString.charAt(1) == 0) CactusSpeed = ScoreString.charAt(0) - ScoreString.charAt(1) / 10 + 4
+    if (Dead != true) if (Paused == false) {
     requestAnimationFrame(update)
     }
 }
